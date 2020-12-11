8ae3f5e84a1565facc7e3d585a9c780e308b6020
adamw <adam@warski.org>
2020-12-11 11:56:58 +0100

    Scala3: shortening

--- scala3/src/main/scala/SlackAlphabet.scala
+++ scala3/src/main/scala/SlackAlphabet.scala
@@ -1,16 +1,12 @@
 import java.util.Locale
 import scala.annotation.tailrec
 
-@main def slackAlphabet(): Unit =
-  println("Hello, world! Type in the text to translate.")
-
-  @tailrec def run(): Unit = 
-    val input = scala.io.StdIn.readLine()
-    input.toLowerCase(Locale.ENGLISH).toList.foreach { c =>
-      if (c == ' ') print("  ")
-      else if (c >= 'a' && c <= 'z') print(s":alphabet-white-$c: ")
-    }
-    println()
-    run()
-  
-  run()
+@main @tailrec def slackAlphabet(): Unit =
+  println("Type in the text to translate:")
+  val input = scala.io.StdIn.readLine()
+  input.toLowerCase(Locale.ENGLISH).toList.foreach { c =>
+    if (c == ' ') print("  ")
+    else if (c >= 'a' && c <= 'z') print(s":alphabet-white-$c: ")
+  }
+  println()
+  slackAlphabet()
